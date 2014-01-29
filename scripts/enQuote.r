47d65c493aeeb40352fa57dd69a0aa1edf20e32a
Sachko Honda <pickledcucumber@gmail.com>
2014-01-29 15:25:46 -0800

    bulkTseries

---
+++ scripts/enQuote.r
@@ -0,0 +1,9 @@
+enQuote <- function( items ){
+    return(paste("'", items, "'", sep=""))
+}
+enQuote2 <- function( items) {
+    return(paste("\"", items, "\"", sep=""))
+}
+enParen <- function( items ){
+    return(paste("(", items, ")", sep=""))
+}
