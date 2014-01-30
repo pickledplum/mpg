c01533a516d63f1ac7ce60a251f19e741008894b
Sachko Honda <pickledcucumber@gmail.com>
2014-01-29 17:41:57 -0800

    getBulkTSeries

--- scripts/getCompanyInfo.r
+++ scripts/getCompanyInfo.r
@@ -40,29 +40,83 @@ getAvailableFQLs <- function(conn, fsid){
     return(data)
 }
 
+findTablename <- function(conn, fsid, fql) {
+    str <- "factset_id='FSID' AND fql='FQL'"
+    str <- gsub("FSID", fsid, str)
+    str <- gsub("FQL", fql, str)
+    ret <- trySelect(conn, "catalog", 
+                     c("tablename"), 
+                     str)
+    if( is.empty(ret) ) return("")
+    return(ret$tablename)
+}
 getTSeries <- function(conn, fsid, fql, t0, t1){
 
     j0 <- julianday(as.Date(t0))
     j1 <- julianday(as.Date(t1))
 
-    ret <- trySelect(conn, "catalog", 
-              c("tablename"), 
-              paste("factset_id='", fsid, "' AND fql='", fql, "'", sep=""))
-    return(ret$tablename)
+    tablename <- findTablename(conn, fsid, fql)
+    str <- "SELECT date(date) AS date, usd FROM 'TABLENAME' WHERE date BETWEEN J0 AND J1"
+    str <- gsub("TABLENAME", tablename, str)
+    str <- gsub("J0", j0, str)
+    str <- gsub("J1", j1, str)
+    ret <- tryGetQuery(conn, str)
+    
+    return(ret)
 }
+
+getBulkTSeries <- function(conn, fsid_list, fql, t0, t1) {
+    j0 <- julianday(as.Date(t0))
+    j1 <- julianday(as.Date(t1))
+    temptable <- paste("bulk_", fql, sep="")
+    trySendQuery(conn, paste("DROP TABLE IF EXISTS", temptable))
+    tryCreateTable(conn, temptable, c("date", enQuote(fsid_list)))
+    
+    for(fsid in fsid_list){
+        tseries <- getTSeries(conn, fsid, fql, t0, t1)
+        if( is.empty(tseries) ){
+            logger.warn(paste("No data:", fsid, fql, "between", t0, "and", t1))
+            next
+        }
+        for( k in seq(1, length(tseries$date)) ){
+            date <- tseries$date[k]
+            jdate <- julianday(as.Date(date))
+            val <- tseries$usd[k]
+            ret <- trySelect(conn, temptable, c("date"), paste("date=", jdate, sep=""))
+            if( is.empty(ret) ){
+                #insert
+                tryInsert(conn, temptable, c("date", fsid),
+                          data.frame(c(jdate), c(val))
+                )
+            } else{
+                str <- "UPDATE TABLENAME SET COLUMNNAME=VALUE WHERE date=JULIANDATE"
+                str <- gsub("TABLENAME", enQuote2(temptable), str)
+                str <- gsub("COLUMNNAME", enQuote2(fsid), str)
+                str <- gsub("VALUE", val, str)
+                str <- gsub("JULIANDATE", jdate, str)
+                trySendQuery(conn, str)
+            }
+        }
+    }
+    ret <- trySelect(conn, temptable, c("date(date) as date", enQuote2(fsid_list)), c())
+    return(ret)
+}
+
 
 db <- "/home/honda/sqlite-db/mini.sqlite"
 conn <<- dbConnect( SQLite(), db )
 print(paste("Opened SQLite database:", db))
 
-universe <- c("GOOG", "JP3902400005", "00105510")
+universe <- c("GOOG", "JP3902400005", "00105510", "004561")
 
 logger.init(level=logger.DEBUG,
             do_stdout=TRUE)
 
-print(paste("Log file:", logfile))
 
 r1 <- getCompanyInfo(conn, universe)
 r2 <- getAvailableFQLs(conn, universe[3])
-r3 <- getTSeries(conn, universe[3], "FF_ASSETS", unjulianday(244330), unjulianday(2445791))
+r3 <- getTSeries(conn, universe[2], "FF_ASSETS", "2010-01-01", "2013-12-31")
+r4 <- getBulkTSeries(conn, universe, "FF_ASSETS", "2010-01-01", "2013-12-31")
+
+
 dbDisconnect(conn)
