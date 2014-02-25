3cfba0e8e82acc51205f5bf59fe8644082d9368a
Sachko Honda <pickledcucumber@gmail.com>
2014-02-25 13:46:02 -0800

    test.na2null.r

--- scripts/na2null.r
+++ scripts/na2null.r
@@ -21,12 +21,3 @@ na2null <- function( str ){
     str <- gsub(", *NA *[)]", ",NULL)", str)
     return(str)
 }
-
-source("assert.r")
-expected = "INSERT OR REPLACE INTO 'P_PRICE_AVG-B3DCPD' ('date','usd','local') VALUES (1,2,NA),(3,4,NA),(5,6,NA),(7, 8,NA)"
-actual = "INSERT OR REPLACE INTO 'P_PRICE_AVG-B3DCPD' ('date','usd','local') VALUES (1,2,NA),(3,4,NA),(5,6,NA),(7, 8,NA)"
-assert.equal(actual, expected)
-
-expected = "NULL,1,2,NULL,3,NULL"
-actual = na2null(paste(c(NA,1,2,NA,3,NA), collapse=","))
-assert.equal(actual, expected)
